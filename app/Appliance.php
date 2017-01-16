a61f9865483ca442b327716feedcbfe9683aa0b5
arthuryyx <arthuryyx@hotmail.com>
2017-01-17 11:13:37 +1300

    update appliance model

--- app/Appliance.php
+++ app/Appliance.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Appliance extends Model
 {
+
+    protected $table = 'appliances';
+
+    protected $fillable = ['name', 'brand_id', 'category_id', 'model', ' description', 'best', 'rrp', 'promotion', 'cutout'];
+
     public function belongsToBrand()
     {
         return $this->belongsTo('App\Brand', 'brand_id', 'id');
