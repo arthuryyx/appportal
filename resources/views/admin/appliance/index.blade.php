3ed57b8c7fe41bea5788915d2a8d01b180a50ff0
arthuryyx <arthuryyx@hotmail.com>
2017-02-01 17:56:39 +1300

    add datetables lib apply it to appliance.index

--- resources/views/admin/appliance/index.blade.php
+++ resources/views/admin/appliance/index.blade.php
@@ -1,87 +1,120 @@
 @extends('layouts.app')
 
+@section('css')
+    <!-- DataTables CSS -->
+    <link href="/vendor/datatables-plugins/dataTables.bootstrap.css" rel="stylesheet">
+
+    <!-- DataTables Responsive CSS -->
+    <link href="/vendor/datatables-responsive/dataTables.responsive.css" rel="stylesheet">
+@endsection
+
 @section('content')
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading"><h3>Appliance Management</h3></div>
-                <div class="panel-body">
-
-                    @if (count($errors) > 0)
-                        <div class="alert alert-danger">
-                            {!! implode('<br>', $errors->all()) !!}
-                        </div>
-                    @endif
-
-                    <a href="{{ url('admin/appliance/create') }}" class="btn btn-lg btn-primary">新增</a>
-
-                    <hr>
-                    <div class="table-responsive">
-                        <table class="table table-bordered table-striped">
-                            <thead>
-                            <tr>
-                                <th>
-                                    Name
-                                </th>
-                                <th>
-                                    model
-                                </th>
-                                <th>
-                                    Brand
-                                </th>
-                                <th>
-                                    Category
-                                </th>
-                                <th>
-                                    best
-                                </th>
-                                <th>
-                                    rrp
-                                </th>
-                                <th>
-                                    promotion
-                                </th>
-                                <th>
-                                    cut-out
-                                </th>
-                                <th>
-                                    description
-                                </th>
-                                <th>
-
-                                </th>
-                                <th>
-
-                                </th>
-                            </tr>
-                            </thead>
-
-                            <tbody>
-                            @foreach ($appliances as $appliance)
-                                <tr>
-                                    <td>{{ $appliance->name }}</td>
-                                    <td>{{ $appliance->model }}</td>
-                                    <td>{{ $appliance->belongsToBrand->name }}</td>
-                                    <td>{{ $appliance->belongsToCategory->name }}</td>
-                                    <td>{{ $appliance->best }}</td>
-                                    <td>{{ $appliance->rrp }}</td>
-                                    <td>{{ $appliance->promotion }}</td>
-                                    <td>{{ $appliance->cutout }}</td>
-                                    <td>{{ $appliance->description }}</td>
-                                    <td><a href="{{ url('admin/appliance/'.$appliance->id.'/edit') }}" class="btn btn-success">编辑</a></td>
-                                    <td><form action="{{ url('admin/appliance/'.$appliance->id) }}" method="POST" style="display: inline;">
-                                            {{ method_field('DELETE') }}
-                                            {{ csrf_field() }}
-                                            <button type="submit" class="btn btn-danger">删除</button>
-                                        </form>
-                                    </td>
-                                </tr>
-                            @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
+        <div class="col-lg-12">
+            <h1 class="page-header">Appliance Management</h1>
         </div>
+        <!-- /.col-lg-12 -->
     </div>
+    <!-- /.row -->
+
+    @if (count($errors) > 0)
+        <div class="alert alert-danger">
+            {!! implode('<br>', $errors->all()) !!}
+        </div>
+    @endif
+
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <a href="{{ url('admin/appliance/create') }}" class="btn btn-primary ">Add</a>
+                </div>
+                <!-- /.panel-heading -->
+                <div class="panel-body">
+                    <table width="100%" class="table table-striped table-bordered table-hover" id="dataTables">
+                        <thead>
+                        <tr>
+                            <th>
+                                Name
+                            </th>
+                            <th>
+                                model
+                            </th>
+                            <th>
+                                Brand
+                            </th>
+                            <th>
+                                Category
+                            </th>
+                            <th>
+                                best
+                            </th>
+                            <th>
+                                rrp
+                            </th>
+                            <th>
+                                promotion
+                            </th>
+                            <th>
+                                cut-out
+                            </th>
+                            <th>
+                                description
+                            </th>
+                            <th>
+
+                            </th>
+                            <th>
+
+                            </th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach ($appliances as $appliance)
+                            <tr>
+                                <td>{{ $appliance->name }}</td>
+                                <td>{{ $appliance->model }}</td>
+                                <td>{{ $appliance->belongsToBrand->name }}</td>
+                                <td>{{ $appliance->belongsToCategory->name }}</td>
+                                <td>{{ $appliance->best }}</td>
+                                <td>{{ $appliance->rrp }}</td>
+                                <td>{{ $appliance->promotion }}</td>
+                                <td>{{ $appliance->cutout }}</td>
+                                <td>{{ $appliance->description }}</td>
+                                <td><a href="{{ url('admin/appliance/'.$appliance->id.'/edit') }}" class="btn btn-success">编辑</a></td>
+                                <td><form action="{{ url('admin/appliance/'.$appliance->id) }}" method="POST" style="display: inline;">
+                                        {{ method_field('DELETE') }}
+                                        {{ csrf_field() }}
+                                        <button type="submit" class="btn btn-danger">删除</button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                    <!-- /.table-responsive -->
+                </div>
+                <!-- /.panel-body -->
+            </div>
+            <!-- /.panel -->
+        </div>
+        <!-- /.col-lg-12 -->
+    </div>
+    <!-- /.row -->
+
+@endsection
+
+@section('js')
+    <!-- DataTables JavaScript -->
+    <script src="/vendor/datatables/js/jquery.dataTables.min.js"></script>
+    <script src="/vendor/datatables-plugins/dataTables.bootstrap.min.js"></script>
+    <script src="/vendor/datatables-responsive/dataTables.responsive.js"></script>
+    <!-- Page-Level Demo Scripts - Tables - Use for reference -->
+    <script>
+        $(document).ready(function() {
+            $('#dataTables').DataTable({
+                responsive: true
+            });
+        });
+    </script>
 @endsection
