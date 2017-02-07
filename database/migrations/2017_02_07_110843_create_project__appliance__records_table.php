daf3b7402503891ace7e9991e440d861a8871b9e
arthuryyx <arthuryyx@hotmail.com>
2017-02-07 17:05:03 +1300

    project order stock  seven tables' migrations

---
+++ database/migrations/2017_02_07_110843_create_project__appliance__records_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectApplianceRecordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('project_appliance_records', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('project_id')->unsigned();
+            $table->integer('appliance_id')->unsigned();
+            $table->boolean('cancel')->unsigned()->default(false);
+            $table->integer('earnest')->unsigned()->default(0);
+            $table->integer('value')->unsigned();
+
+
+            $table->foreign('project_id')
+                ->references('id')
+                ->on('projects')
+                ->onDelete('cascade');
+
+            $table->foreign('appliance_id')
+                ->references('id')
+                ->on('appliances')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('project__appliance__records');
+    }
+}
---
+++ database/migrations/2017_02_07_125630_create_appliance__orders_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateApplianceOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('appliance_orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('project_id')->unsigned()->nullable();
+
+            $table->foreign('project_id')
+                ->references('id')
+                ->on('projects')
+                ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('appliance__orders');
+    }
+}
