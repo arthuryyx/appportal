78c77f4a8b26136c6470075773a6b0622b8de7d1
arthuryyx <arthuryyx@hotmail.com>
2017-02-23 10:40:01 +1300

    fix Syntax error or access violation: 1071 Specified key was too long

--- config/database.php
+++ config/database.php
@@ -63,7 +63,7 @@ return [
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
             'strict' => true,
-            'engine' => null,
+            'engine' => 'InnoDB ROW_FORMAT=DYNAMIC',
 //            'options'   => [
 //                \PDO::ATTR_EMULATE_PREPARES => true
 //            ],
