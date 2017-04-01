a7862df96b656efd7f12993a9f6188ccc2dca07e
arthuryyx <arthuryyx@hotmail.com>
2017-04-01 14:08:15 +1300

    change a header

--- resources/views/appliance/stock/listing.blade.php
+++ resources/views/appliance/stock/listing.blade.php
@@ -11,7 +11,7 @@
 @section('content')
     <div class="row">
         <div class="col-lg-12">
-            <h1 class="page-header">Ready To Go</h1>
+            <h1 class="page-header">Set Shelf</h1>
         </div>
         <!-- /.col-lg-12 -->
     </div>
