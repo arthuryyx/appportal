8475d9d3bc28bc26575c70c36cad63f85e210ac5
arthuryyx <arthuryyx@hotmail.com>
2017-04-03 17:02:20 +1200

    refactor html

--- resources/views/appliance/stock/index5.blade.php
+++ resources/views/appliance/stock/index5.blade.php
@@ -2,7 +2,11 @@
 
 @section('css')
     <link href="{{ asset('vendor/select2/select2.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('vendor/datatables-checkboxes/dataTables.checkboxes.css') }}" rel="stylesheet">
+    <!-- DataTables CSS -->
+    <link href="{{ asset('vendor/datatables-plugins/dataTables.bootstrap.css')}}" rel="stylesheet">
+
+    <!-- DataTables Responsive CSS -->
+    <link href="{{ asset('vendor/datatables-responsive/dataTables.responsive.css')}}" rel="stylesheet">
 @endsection
 
 @section('content')
@@ -14,73 +18,70 @@
     </div>
     <!-- /.row -->
 
+    @if (count($errors) > 0)
+        <div class="alert alert-danger alert-dismissable">
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+            {!! implode('<br>', $errors->all()) !!}
+        </div>
+    @endif
+
     <div class="row">
         <div class="col-lg-12">
-            @if (count($errors) > 0)
-                <div class="alert alert-danger alert-dismissable">
-                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                    {!! implode('<br>', $errors->all()) !!}
-                </div>
-            @endif
+            @can('root')
             <div class="panel panel-default">
                 <div class="panel-body">
-                    @can('root')
-                    <div class="row">
-                        <div class="col-xs-6 col-sm-6 col-md-6">
-                            {!! Form::open(['url' => 'appliance/stock/display','method'=>'POST']) !!}
-                            <strong>from stock</strong>
-                            <select class="sid form-control" name="sid" required="required"></select>
-                            {{Form::submit('Submit', ['class' => 'btn  add-more btn-success pull-right'])}}
-                            {!! Form::close() !!}
-                        </div>
+                    <div class="col-xs-5">
+                        {!! Form::open(['url' => 'appliance/stock/display','method'=>'POST']) !!}
+                        <strong>from stock</strong>
+                        <select class="sid form-control" name="sid" required="required"></select>
+                        {{Form::submit('Submit', ['class' => 'btn  add-more btn-success pull-right'])}}
+                        {!! Form::close() !!}
                     </div>
-                    @endcan
                 </div>
             </div>
+            @endcan
             <div class="panel panel-default">
-                <div class="panel panel-body">
-                    <form id="frm-example" name="frm_example" action="" method="post">
-                        {{ csrf_field() }}
-                        {{--@can('root')--}}
-                            {{--<button type="submit" class="btn btn-primary" onclick="document.frm_example.action='{{ url('appliance/stock/release')}}'">order</button>--}}
-                        {{--@endcan--}}
-                        <table width="100%" class="table table-striped table-bordered table-hover" id="dataTables">
-                            <thead>
+                <!-- /.panel-heading -->
+                <div class="panel-body">
+                    <table width="100%" class="table table-striped table-bordered table-hover" id="dataTables">
+                        <thead>
+                        <tr>
+                            <th>
+                                Model
+                            </th>
+                            <th>
+                                Brand
+                            </th>
+                            <th>
+                                Category
+                            </th>
+                            <th>
+                                Shelf
+                            </th>
+                            <th></th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach ($stocks as $stock)
                             <tr>
-                                <th></th>
-                                <th>
-                                    Model
-                                </th>
-                                <th>
-                                    Brand
-                                </th>
-                                <th>
-                                    Category
-                                </th>
-                                <th>
-                                    Shelf
-                                </th>
-                                <th></th>
+                                <td>{{ $stock->appliance->model }}</td>
+                                <td>{{ $stock->appliance->belongsToBrand->name }}</td>
+                                <td>{{ $stock->appliance->belongsToCategory->name }}</td>
+                                <td>{{ $stock->shelf }}</td>
+                                <td><a href="{{ url('appliance/stock/'.$stock->id.'/edit') }}" class="btn btn-success">编辑</a></td>
                             </tr>
-                            </thead>
-                            <tbody>
-                            @foreach ($stocks as $stock)
-                                <tr>
-                                    <td>{{ $stock->id }}</td>
-                                    <td>{{ $stock->appliance->model }}</td>
-                                    <td>{{ $stock->appliance->belongsToBrand->name }}</td>
-                                    <td>{{ $stock->appliance->belongsToCategory->name }}</td>
-                                    <td>{{ $stock->shelf }}</td>
-                                    <td><a href="{{ url('appliance/stock/'.$stock->id.'/edit') }}" class="btn btn-success">编辑</a></td>
-                                </tr>
-                            @endforeach
-                            </tbody>
-                        </table>
-                    </form>
+                        @endforeach
+                        </tbody>
+                    </table>
+                    <!-- /.table-responsive -->
                 </div>
+                <!-- /.panel-body -->
             </div>
+            <!-- /.panel -->
         </div>
+        <!-- /.col-lg-12 -->
     </div>
+    <!-- /.row -->
 
 @endsection
 
@@ -109,49 +110,16 @@
         });
 
     </script>
-
     <!-- DataTables JavaScript -->
     <script src="{{ asset('vendor/datatables/js/jquery.dataTables.min.js')}}"></script>
     <script src="{{ asset('vendor/datatables-plugins/dataTables.bootstrap.min.js')}}"></script>
     <script src="{{ asset('vendor/datatables-responsive/dataTables.responsive.js')}}"></script>
-    <script src="{{ asset('vendor/datatables-checkboxes/dataTables.checkboxes.min.js')}}"></script>
     <!-- Page-Level Demo Scripts - Tables - Use for reference -->
     <script>
         $(document).ready(function() {
-            var table = $('#dataTables').DataTable({
+            $('#dataTables').DataTable({
                 responsive: true,
-                paging: false,
-                searching: false,
-                columnDefs: [
-                    {
-                        'targets': 0,
-                        'checkboxes': {
-                            'selectRow': true,
-                            'value': 1
-                        }
-                    }
-                ],
-                select: {
-                    'style': 'multi'
-                },
-                order: [[1, 'asc']]
-            });
-
-            // Handle form submission event
-            $('#frm-example').on('submit', function(e){
-                var form = this;
-                var rows_selected = table.column(0).checkboxes.selected();
-
-                // Iterate over all selected checkboxes
-                $.each(rows_selected, function(index, sid){
-                    // Create a hidden element
-                    $(form).append(
-                        $('<input>')
-                            .attr('type', 'hidden')
-                            .attr('name', 'id[]')
-                            .val(sid)
-                    );
-                });
+                paging: false
             });
         });
     </script>
