d4671db3a6fe76911639d40e20caae7439d0d09d
arthuryyx <arthuryyx@hotmail.com>
2017-05-30 16:06:04 +1200

    statistics pie chart current month sales at homepage

---
+++ app/Http/Controllers/StatisticsController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Role;
+use App\Appliance_Invoice;
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class StatisticsController extends Controller
+{
+    public function salesChart(){
+        $sales = Role::where('name', 'sales')->first()->users()->pluck('id');
+        $array = Appliance_Invoice::whereIn('created_by', $sales)
+            ->whereYear('created_at', date('Y'))
+            ->whereMonth('created_at', date('m'))
+            ->groupBy('created_by')
+            ->select('created_by', DB::raw('sum(price) as value'))
+            ->pluck('value', 'created_by');
+        $data = array();
+        foreach ($array as $key => $value){
+            $data [count($data)]['label']=User::where('id', $key)->select('name')->first()->name;
+            $data [count($data)-1]['value']=$value;
+        }
+        return $data;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -87,11 +87,13 @@ Route::group(['middleware' => 'auth', 'namespace' => 'Appliance', 'prefix' => 'a
 
 
 Route::group(['middleware' => 'auth'], function() {
-    Route::get('tempstock/create', 'StockController@create');
-    Route::post('tempstock', 'StockController@store');
-    Route::get('tempstock/{id}/out', 'StockController@out');
-    Route::delete('tempstock/{id}', 'StockController@destroy');
-    Route::get('tempstock/exportAssigned', 'StockController@exportAssigned');
+//    Route::get('tempstock/create', 'StockController@create');
+//    Route::post('tempstock', 'StockController@store');
+//    Route::get('tempstock/{id}/out', 'StockController@out');
+//    Route::delete('tempstock/{id}', 'StockController@destroy');
+//    Route::get('tempstock/exportAssigned', 'StockController@exportAssigned');
+
+    Route::get('statistics/salesChart', 'StatisticsController@salesChart');
 
     Route::get('settings', 'UserController@settings');
     Route::post('/settings/reset', 'UserController@reset');
