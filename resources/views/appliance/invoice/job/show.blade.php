a5f28fba1bc12191d7bb9715f17781b131529459
arthuryyx <arthuryyx@hotmail.com>
2017-07-20 09:06:34 +1200

    fix wrong title

--- resources/views/appliance/invoice/job/show.blade.php
+++ resources/views/appliance/invoice/job/show.blade.php
@@ -8,7 +8,7 @@
 @section('content')
     <div class="row">
         <div class="col-lg-12">
-            <h1 class="page-header">Quotation Info</h1>
+            <h1 class="page-header">Invoice Info</h1>
         </div>
         <!-- /.col-lg-12 -->
     </div>
